88a37bbb339ae4a059b181adb194e185f5ff13e3
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-02-18 16:53:04 -0600

    Initialized a SessionFactory

--- src/main/java/com/teamtreehouse/contactmgr/Application.java
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -1,6 +1,21 @@
 package com.teamtreehouse.contactmgr;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
 public class Application {
+    // Hold a reusable to a SessionFactory
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        // Create a StandardServiceRegistry
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
     public static void main(String[] args) {
 
     }
