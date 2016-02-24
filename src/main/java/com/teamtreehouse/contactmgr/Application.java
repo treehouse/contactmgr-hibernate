99f21418e9c8d49ce2da2c89803fddadb33e10a0
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-02-24 13:04:53 -0600

    Persisted a Contact object with Hibernate

--- src/main/java/com/teamtreehouse/contactmgr/Application.java
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -2,6 +2,7 @@ package com.teamtreehouse.contactmgr;
 
 import com.teamtreehouse.contactmgr.model.Contact;
 import com.teamtreehouse.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -9,7 +10,7 @@ import org.hibernate.service.ServiceRegistry;
 
 public class Application {
     // Hold a reusable reference to a SessionFactory (since we need only one)
-    //private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         // Create a StandardServiceRegistry
@@ -22,6 +23,19 @@ public class Application {
                 .withEmail("rama@teamtreehouse.com")
                 .withPhone(7735556666L)
                 .build();
-        System.out.println(contact);
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Use the session to save the contact
+        session.save(contact);
+
+        // Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
     }
 }
