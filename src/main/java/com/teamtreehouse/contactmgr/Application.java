64e369a9be60ba4825bd2afd1ed26de296b98e3d
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-02-24 13:39:25 -0600

    Retrieved a list of persisted objects using a Hibernate Criteria object

--- src/main/java/com/teamtreehouse/contactmgr/Application.java
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -2,11 +2,14 @@ package com.teamtreehouse.contactmgr;
 
 import com.teamtreehouse.contactmgr.model.Contact;
 import com.teamtreehouse.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
 
 public class Application {
     // Hold a reusable reference to a SessionFactory (since we need only one)
@@ -23,6 +26,30 @@ public class Application {
                 .withEmail("rama@teamtreehouse.com")
                 .withPhone(7735556666L)
                 .build();
+        save(contact);
+
+        // Display a list of contacts
+        fetchAllContacts().stream().forEach(System.out::println);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Contact> fetchAllContacts() {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Create Criteria
+        Criteria criteria = session.createCriteria(Contact.class);
+
+        // Get a list of Contact objects according to the Criteria object
+        List<Contact> contacts = criteria.list();
+
+        // Close the session
+        session.close();
+
+        return contacts;
+    }
+
+    private static void save(Contact contact) {
         // Open a session
         Session session = sessionFactory.openSession();
 
