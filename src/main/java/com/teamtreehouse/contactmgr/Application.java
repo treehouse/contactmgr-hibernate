758f58b94582d6a490582af9bc2fd34d6d350209
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-02-24 10:15:54 -0600

    Added Contact class with JPA annotations and mapping element to Hibernate config

---
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -0,0 +1,7 @@
+package com.teamtreehouse.contactmgr;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/teamtreehouse/contactmgr/model/Contact.java
@@ -0,0 +1,76 @@
+package com.teamtreehouse.contactmgr.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Column
+    private Long phone;
+
+    // Default constructor for JPA
+    public Contact(){}
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+}
